0e9f51fd19ee3d94e07b4fc41508306252f86eca
Sergey Lyashev <ser59617710@mail.ru>
2025-04-07 11:34:57 +0300

    add test_external_api.py 12.1

---
+++ tests/test_utils.py
@@ -0,0 +1,37 @@
+import pytest
+from src.utils import get_transactions
+import os.path
+from config import DATA_DIR
+from unittest.mock import patch, mock_open
+import json
+
+
+@patch('builtins.open', new_callable=mock_open, read_data='[1, 2, 3]')
+@patch('json.load')
+def test_get_transactions(mock_json_load, mock_open):
+    '''Тестирование преобразования JSON-файла в список'''
+
+    # Настраиваем mock для json.load с нормальными данными
+    mock_json_load.return_value = [1, 2, 3]
+    # Вызываем функцию и проверяем результат
+    result = get_transactions('fake_file.json')
+    assert result == [1, 2, 3]
+
+    # Проверяем, что open был вызван с правильными аргументами
+    mock_open.assert_called_once_with('fake_file.json', 'r', encoding='utf-8')
+
+    # Проверяем, что json.load был вызван
+    mock_json_load.assert_called_once()
+
+
+pathname_1 = os.path.join(DATA_DIR, 'test.json')
+pathname_2 = os.path.join(DATA_DIR, 'test_operation.json')
+
+@pytest.mark.parametrize("variable, expected",
+                             [(pathname_1, []),
+                              (pathname_2, [])])
+def test_get_bad_transactions(variable, expected):
+    '''Тестирование преобразования JSON-файла в список
+    1 - тест когда файл не найден,
+    2 - тест с файлом не JSON'''
+    assert get_transactions(variable) == expected
